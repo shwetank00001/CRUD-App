f4abf8402db6118a1b16a7445041a361b28f18d2
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-03-11 16:55:07 +0530

    update 1

--- src/Create.js
+++ src/Create.js
@@ -2,11 +2,21 @@ import React from 'react'
 
 const Create = () => {
 
-  const [form ,setForm] = React.useState('')
+  const [form ,setForm] = React.useState({
+    fullName:"",
+    email:"",
+    phone:""
+  })
+
+  const [display, setDisplay] = React.useState([])
 
   function handleSubmit(e){
     e.preventDefault()
     console.log(form)
+    if(form.name && form.email && form.phone){
+      const newData = {...form}
+      setDisplay([...display, newData])
+    }
   }
 
   function handleChange(e){
@@ -18,6 +28,16 @@ const Create = () => {
       })
     })
   }
+
+  const ele = display.map(function(form,item){
+    return(
+      <div key={item}>
+        <h4>{form.fullName}</h4>
+        <h4>{form.email}</h4>
+        <h4>{form.phone}</h4>
+      </div>
+    )
+  })
   
   return (
     <div className='div-create'>
@@ -26,7 +46,7 @@ const Create = () => {
         <form onSubmit={handleSubmit}>
 
           <label htmlFor='name'>Name</label>          <br/>
-          <input type="text" id='name' name='fullName' value={form.name} onChange={handleChange} />
+          <input type="text" id='name' name='fullName' value={form.fullName} onChange={handleChange} />
 
           <br/>
           <br/>
@@ -40,9 +60,12 @@ const Create = () => {
           <input type="text" id='phone' name='phone' value={form.phone} onChange={handleChange}  />
           <br />
           <br />
-
           <button>Add</button>
         </form>
+
+        <div>
+          {ele}
+        </div>
     </div>
   )
 }
